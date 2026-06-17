389e62540592eec4c8a3bd5451765fb44a393ec5
Timothy Ododo <timothyododo@gmail.com>
2026-06-17 18:05:24 +0100

    restructured to incooparate web version and CLI and at the same time

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"asciiArt/ascii"
+	"asciiArt/handler"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
 
 func main() {
+
+	// Web Entry Point
+	fmt.Println("Starting Server...")
+
+	router := http.NewServeMux()
+
+	router.Handle("GET /", http.HandlerFunc(handler.HomeHandler))
+	router.Handle("POST /ascii-art", http.HandlerFunc(handler.ProcessHandler))
+
+	server := http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	startServerError := server.ListenAndServe()
+
+	if startServerError != nil {
+		fmt.Println(startServerError)
+	} else {
+		fmt.Printf("Server successfuly started on Port %v", &server.Addr)
+	}
+
+	//CLI Entry Point
 	if len(os.Args) < 2 {
 		fmt.Println(fmt.Errorf("USAGE: go run . Hello standard"))
 
@@ -21,13 +47,17 @@ func main() {
 			BannerFile = BannerFile + ".txt"
 		}
 
-		loadBannerFile, err := loadBanner(BannerFile)
+		loadBannerFile, err := ascii.LoadBanner(BannerFile)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		AsciiCharacter, err := generateAscii(TextInput, loadBannerFile)
+		// split input Text input from command Line
+		textInput := strings.ReplaceAll(TextInput, "\r\n", "\n")
+		splitInput := strings.Split(textInput, "\\n")
+
+		AsciiCharacter, err := ascii.GenerateAscii(splitInput, loadBannerFile)
 		if err != nil {
 			fmt.Println(err)
 			return
